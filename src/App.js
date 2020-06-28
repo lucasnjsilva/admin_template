312407644a235e9886b8742f24df52d9bd489ad6
lucasnjsilva <lucasnathanj@gmail.com>
2020-06-28 00:10:11 -0300

    Created the route file and organize the folder structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import AppLayout from './containers/AppLayout';
-import Login from './components/Login';
-import Forgot from './components/Forgot';
-import Register from './components/Register';
-import Error404 from './components/Error404';
+import Routes from './routes';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" name="Home" render={props => <AppLayout {...props} />} />
-        <Route exact path="/login" name="Login" render={props => <Login {...props} />} />
-        <Route exact path="/forgot" name="Forgot" render={props => <Forgot {...props} />} />
-        <Route exact path="/register" name="Register" render={props => <Register {...props} />} />
-
-        {/* Error 404 route. */}                
-        <Route path="*" name="Error404" render={props => <Error404 {...props} />} />
-
-      </Switch>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+    <div className="App">
+      <Routes />
+    </div>
+);
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AppLayout from './containers/AppLayout';
+import Login from './components/Login';
+import Forgot from './components/Forgot';
+import Register from './components/Register';
+import Error404 from './components/Error404';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" name="Home" render={props => <AppLayout {...props} />} />
+            <Route exact path="/login" name="Login" render={props => <Login {...props} />} />
+            <Route exact path="/forgot" name="Forgot" render={props => <Forgot {...props} />} />
+            <Route exact path="/register" name="Register" render={props => <Register {...props} />} />
+
+            {/* Error 404 route. */}                
+            <Route path="*" name="Error404" render={props => <Error404 {...props} />} />
+        </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;

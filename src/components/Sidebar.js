e8d49694c1a3b25c631c8671d6e5362a0007fb51
lucasnjsilva <lucasnathanj@gmail.com>
2020-06-30 01:10:29 -0300

    sidebar added

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Sidebar(props) {
+    return (
+        <aside id="sidebar">
+            <div className="sidebar-title">
+                <h2>School of Net</h2>
+            </div>
+            <div className="list-group">
+                <a href="#1" className="list-group-item"> Link </a>
+            </div>
+        </aside>
+    );
+}
--- src/containers/AppLayout.js
+++ src/containers/AppLayout.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Templates from './LoadingRoutes';
+import Header from './../components/Header';
+import Sidebar from './../components/Sidebar';
 
 export default class AppLayout extends Component {
     render() {
         return(
-            <div>
-                HEADER
-                <Switch>
-                    {Templates.map((route, index) => (
-                        <Route path={route.path} exact render={props => <route.template {...props} />} />
-                    ))}
-                    
-                </Switch>
+            <div className="d-flex" id="main-wrapper">
+                <Sidebar />
+                <div id="content-wrapper">
+                    <Header />
+                    <Switch>
+                        {Templates.map((route, index) => (
+                            <div className="content-dynamic">
+                                <Route key={index} path={route.path} exact render={props => <route.template {...props} />} />
+                            </div>
+                        ))}
+                        
+                    </Switch>
+                </div>
             </div>            
         );
     }
